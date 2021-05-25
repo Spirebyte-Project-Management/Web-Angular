175ca48bc5ee33f6954fb1f8f4bdabb5db727088
Jordi Huntjens <442216@student.fontys.nl>
2021-05-25 20:17:52 +0200

    Remove /

--- Spirebyte.Web.Angular/ClientApp/src/environments/environment.prod.ts
+++ Spirebyte.Web.Angular/ClientApp/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   appVersion: 'v711demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
-  apiUrl: 'https://api.spirebyte.nl/',
+  apiUrl: 'https://api.spirebyte.nl',
   websocketUrl: 'http://164.68.108.142:5006',
 
   defaultPermissionSchemeId: '11111111-1111-1111-1111-111111111111'
