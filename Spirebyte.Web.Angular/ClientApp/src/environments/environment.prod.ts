f7c4eacd7a119942ca02d347514e85ad7bfbebdd
Jordi Huntjens <442216@student.fontys.nl>
2021-05-25 20:17:32 +0200

    Added production urls

--- Spirebyte.Web.Angular/ClientApp/src/environments/environment.prod.ts
+++ Spirebyte.Web.Angular/ClientApp/src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   appVersion: 'v711demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
-  apiUrl: 'http://localhost:5000',
-  websocketUrl: 'http://localhost:5006',
+  apiUrl: 'https://api.spirebyte.nl/',
+  websocketUrl: 'http://164.68.108.142:5006',
 
   defaultPermissionSchemeId: '11111111-1111-1111-1111-111111111111'
   // apiUrl: 'mysite.com/api'
